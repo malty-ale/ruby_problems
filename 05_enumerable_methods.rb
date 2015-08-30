d2d88e0127255b756be2b2d2bf0a56b23cd0af40
malty-ale <eric.maltiel@gmail.com>
2015-08-30 16:40:47 +0000

    finish enumerable methods

--- 05_enumerable_methods.rb
+++ 05_enumerable_methods.rb
@@ -14,4 +14,81 @@ module Enumerable
     self
   end
   
+  def my_select
+    result = []
+    self.my_each do |i|
+      result << i if yield(i)
+    end
+    result
+  end
+  
+  def my_all?
+    result = true
+    self.my_each do |i|
+      if !yield(i)
+        result = false
+        break
+      end
+    end
+    result
+  end
+  
+  def my_any?
+    result = false
+    self.my_each do |i|
+      if yield(i)
+        result = true
+        break
+      end
+    end
+    result
+  end
+  
+  def my_none?
+    result = true
+    self.my_each do |i|
+      if yield(i)
+        result = false
+        break
+      end
+    end
+    result
+  end
+  
+  def my_count
+    count = 0
+    self.my_each do |i|
+      if yield(i)
+        count += 1
+      end
+    end
+    count
+  end
+  
+  def my_map(input_proc)
+    result = []
+    self.my_each do |i|
+      if block_given?
+        result << yield(input_proc.call(i))
+      else
+        result << input_proc.call(i)
+      end
+    end
+    result
+  end
+  
+  def my_inject(n=nil)
+    array = self.to_a
+    result = n == nil ? array.first : n
+    range = n == nil ? (1...array.length).to_a : (0...array.length).to_a
+    range.my_each do |i|
+      result = yield(result, array[i])
+    end
+    result
+  end
+  
+end
+
+def multiply_els(array)
+  array.my_inject{|i,j| i*j}
 end
